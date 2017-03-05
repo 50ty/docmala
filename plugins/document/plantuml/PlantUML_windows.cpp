a59a56090ff600d899100799ea0d9f919c264228
Michael Adam <Michael Adam>
2017-03-05 14:54:00 +0100

    Use hosted PlantUML and evaluate errors in PlantUML correctly

--- plugins/document/plantuml/PlantUML_windows.cpp
+++ plugins/document/plantuml/PlantUML_windows.cpp
@@ -9,12 +9,13 @@
 #include <sstream>
 #include <unordered_map>
 
-
 using namespace docmala;
 
 class PlantUMLPlugin : public DocumentPlugin {
     // DocmaPlugin interface
 public:
+    bool initHost(const ParameterList &parameters, const FileLocation &location);
+    ~PlantUMLPlugin() override;
     BlockProcessing blockProcessing() const override;
     bool process( const ParameterList &parameters, const FileLocation &location, Document &document, const std::string &block) override;
 
@@ -22,52 +23,42 @@ public:
 
     std::vector<Error> _errors;
     std::unordered_map<std::string, DocumentPart::Image> _cache;
+
+    HANDLE _stdInWriteHandle = NULL;
+    HANDLE _stdOutReadHandle = NULL;
+    HANDLE _stdErrReadHandle = NULL;
+
+    bool _hostInitialized = false;
 };
 
 
-DocumentPlugin::BlockProcessing PlantUMLPlugin::blockProcessing() const {
-    return BlockProcessing::Required;
+void split(const std::string& s, char c, std::vector<std::string>& v) {
+   std::string::size_type i = 0;
+   std::string::size_type j = s.find(c);
+
+   while (j != std::string::npos) {
+      v.push_back(s.substr(i, j-i-1));
+      i = ++j;
+      j = s.find(c, j);
+
+      if (j == std::string::npos)
+         v.push_back(s.substr(i, s.length()));
+   }
 }
 
-bool PlantUMLPlugin::process(const ParameterList &parameters, const FileLocation &location, Document &document, const std::string &block)
+bool PlantUMLPlugin::initHost(const ParameterList &parameters, const FileLocation &location)
 {
-    std::stringstream outputFileName;
-
-    auto cachePosition = _cache.find(block);
-    std::string pluginDir;
-
-    if( cachePosition != _cache.end() ) {
-        DocumentPart::Image image = cachePosition->second;
-        image.line = location.line;
-        document.addPart( image );
+    if( _hostInitialized )
         return true;
-    }
 
-    auto outPathIter = parameters.find("outputDir");
-    if( outPathIter != parameters.end() ) {
-        outputFileName << outPathIter->second.value << "/";
-    }
+    std::string pluginDir;
 
     auto pluginDirIter = parameters.find("pluginDir");
     if( pluginDirIter != parameters.end() ) {
         pluginDir =  pluginDirIter->second.value + '/';
     }
 
-    outputFileName << "uml_" << "_" << location.line <<".png";
-
-    std::string outFile = "@startuml\n";
-    outFile += block + "\n";
-    outFile += "@enduml \n";
-
-    int exit_code = 0;
-
-    HANDLE g_hChildStd_IN_Rd = NULL;
-    HANDLE g_hChildStd_IN_Wr = NULL;
-    HANDLE g_hChildStd_OUT_Rd = NULL;
-    HANDLE g_hChildStd_OUT_Wr = NULL;
-
-    //std::string stringargs [] = {"java", "-jar", pluginDir + "plantuml.jar", "-p"};
-    std::string commandline = "javaw -splash:no -jar " + pluginDir + "plantuml.jar -p -nbthreads 4";
+    std::string commandLine = "javaw -splash:no -jar " + pluginDir + "PlantUMLHost.jar";
 
     PROCESS_INFORMATION piProcInfo;
     STARTUPINFO siStartInfo;
@@ -77,119 +68,184 @@ bool PlantUMLPlugin::process(const ParameterList &parameters, const FileLocation
     saAttr.bInheritHandle = TRUE;
     saAttr.lpSecurityDescriptor = NULL;
 
-    BOOL bSuccess;
+    HANDLE stdInReadHandle = NULL;
+    HANDLE stdOutWriteHandle = NULL;
+    HANDLE stdErrWriteHandle = NULL;
+
 
     ZeroMemory( &piProcInfo, sizeof(PROCESS_INFORMATION) );
     ZeroMemory( &siStartInfo, sizeof(STARTUPINFO) );
 
     // Create a pipe for the child process's STDOUT.
-
-    if ( ! CreatePipe(&g_hChildStd_OUT_Rd, &g_hChildStd_OUT_Wr, &saAttr, 0) ) {
-        _errors.push_back({location, "StdoutRd CreatePipe"});
+    if ( !CreatePipe(&_stdOutReadHandle, &stdOutWriteHandle, &saAttr, 0) ) {
+        _errors.push_back({location, "Unable to create pipe for stdOut"});
         return false;
     }
 
     // Ensure the read handle to the pipe for STDOUT is not inherited.
-
-    if ( ! SetHandleInformation(g_hChildStd_OUT_Rd, HANDLE_FLAG_INHERIT, 0) ) {
-        _errors.push_back({location, "StdoutRd SetHandleInformation"});
+    if ( !SetHandleInformation(_stdOutReadHandle, HANDLE_FLAG_INHERIT, 0) ) {
+        _errors.push_back({location, "Unable to configure pipe for stdOut"});
         return false;
     }
 
     // Create a pipe for the child process's STDIN.
-
-    if (! CreatePipe(&g_hChildStd_IN_Rd, &g_hChildStd_IN_Wr, &saAttr, 0)) {
-        _errors.push_back({location, "Stdin CreatePipe"});
+    if ( !CreatePipe(&stdInReadHandle, &_stdInWriteHandle, &saAttr, 0) ) {
+        _errors.push_back({location, "Unable to create pipe for stdIn"});
         return false;
     }
 
     // Ensure the write handle to the pipe for STDIN is not inherited.
+    if ( ! SetHandleInformation(_stdInWriteHandle, HANDLE_FLAG_INHERIT, 0) ) {
+        _errors.push_back({location, "Unable to configure pipe for stdIn"});
+        return false;
+    }
 
-    if ( ! SetHandleInformation(g_hChildStd_IN_Wr, HANDLE_FLAG_INHERIT, 0) ) {
-        _errors.push_back({location, "Stdin SetHandleInformation"});
+    // Create a pipe for the child process's STDERR.
+    if ( !CreatePipe(&_stdErrReadHandle, &stdErrWriteHandle, &saAttr, 0) ) {
+        _errors.push_back({location, "Unable to create pipe for stdErr"});
+        return false;
+    }
+
+    // Ensure the read handle to the pipe for STDERR is not inherited.
+    if ( !SetHandleInformation(_stdErrReadHandle, HANDLE_FLAG_INHERIT, 0) ) {
+        _errors.push_back({location, "Unable to configure pipe for stdErr"});
         return false;
     }
 
     siStartInfo.cb = sizeof(STARTUPINFO);
-    //siStartInfo.hStdError = g_hChildStd_OUT_Wr;
-    siStartInfo.hStdOutput = g_hChildStd_OUT_Wr;
-    siStartInfo.hStdInput = g_hChildStd_IN_Rd;
+    siStartInfo.hStdError = stdErrWriteHandle;
+    siStartInfo.hStdOutput = stdOutWriteHandle;
+    siStartInfo.hStdInput = stdInReadHandle;
     siStartInfo.dwFlags |= STARTF_USESTDHANDLES;
 
-    bSuccess = CreateProcessA(
+    if( !CreateProcessA(
                 NULL,
-                &commandline[0],     // command line
-            NULL,          // process security attributes
-            NULL,          // primary thread security attributes
-            TRUE,          // handles are inherited
-            0,             // creation flags
-            NULL,          // use parent's environment
-            NULL,          // use parent's current directory
-            &siStartInfo,  // STARTUPINFO pointer
-            &piProcInfo);  // receives PROCESS_INFORMATION
-
-    if( !bSuccess ) {
-        _errors.push_back({location, "Unable to execute: '" + commandline + "'"});
+                &commandLine[0],     // command line
+                NULL,          // process security attributes
+                NULL,          // primary thread security attributes
+                TRUE,          // handles are inherited
+                0,             // creation flags
+                NULL,          // use parent's environment
+                NULL,          // use parent's current directory
+                &siStartInfo,  // STARTUPINFO pointer
+                &piProcInfo) ) { // receives PROCESS_INFORMATION
+        _errors.push_back({location, "Unable to execute: '" + commandLine + "'"});
         return false;
     }
 
+    CloseHandle(stdInReadHandle);
+    CloseHandle(stdOutWriteHandle);
+    CloseHandle(stdErrWriteHandle);
+
+    _hostInitialized = true;
+    return true;
+}
+
+PlantUMLPlugin::~PlantUMLPlugin()
+{
+    CloseHandle(_stdInWriteHandle);
+    CloseHandle(_stdOutReadHandle);
+}
+
+DocumentPlugin::BlockProcessing PlantUMLPlugin::blockProcessing() const {
+    return BlockProcessing::Required;
+}
+
+bool PlantUMLPlugin::process(const ParameterList &parameters, const FileLocation &location, Document &document, const std::string &block)
+{
+    _errors.clear();
+    if( !initHost(parameters, location) ) {
+        return false;
+    }
+
+    auto cachePosition = _cache.find(block);
+
+    if( cachePosition != _cache.end() ) {
+        DocumentPart::Image image = cachePosition->second;
+        image.line = location.line;
+        document.addPart( image );
+        return true;
+    }
+
+    std::string outFile = "@startuml\n";
+    outFile += block + "\n";
+    outFile += "@enduml \n";
+
     DWORD bytesWritten = 0;
     std::size_t bytesToWrite = outFile.length();
+    BOOL bSuccess;
 
     while( bytesToWrite > 0 ) {
         DWORD thisTimeWritten = 0;
-        bSuccess = WriteFile(g_hChildStd_IN_Wr, outFile.data() + bytesWritten, bytesToWrite, &thisTimeWritten, NULL);
+        bSuccess = WriteFile(_stdInWriteHandle, outFile.data() + bytesWritten, bytesToWrite, &thisTimeWritten, NULL);
         if ( ! bSuccess )
             break;
         bytesToWrite -= thisTimeWritten;
         bytesWritten += thisTimeWritten;
     }
 
-    CloseHandle(g_hChildStd_IN_Wr);
-    CloseHandle(g_hChildStd_IN_Rd);
-    CloseHandle(g_hChildStd_OUT_Wr);
-
     std::string imageData;
 
-//    DWORD bytesAvail = 0;
-//    if (!PeekNamedPipe(g_hChildStd_OUT_Rd, NULL, 0, NULL, &bytesAvail, NULL)) {
-//        _errors.push_back({location, "Failed to call PeekNamedPipe" });
-//        return false;
-//    }
+    int32_t length = 0;
+    DWORD readBytes = 0;
 
-//    imageData.resize( static_cast<std::string::size_type>(bytesAvail) );
+    bSuccess = ReadFile( _stdOutReadHandle, &length, 4, &readBytes, NULL);
 
-//    DWORD readBytes = 0;
-//    bSuccess = ReadFile( g_hChildStd_OUT_Rd, &imageData[0], imageData.size(), &readBytes, NULL);
+    size_t pos = 0;
+    imageData.resize(length);
 
-//    if( readBytes != bytesAvail ) {
-//        _errors.push_back({location, "Not all data was read." });
-//        return false;
-//    }
     while( true )
     {
-        char buffer[1024];
+        DWORD readBytes = 0;
+        bSuccess = ReadFile( _stdOutReadHandle, &imageData[pos], length - pos, &readBytes, NULL);
+
+        pos += readBytes;
+
+        if( !bSuccess || readBytes == 0 || pos >= length )
+            break;
+    }
+
+    DocumentPart::Text text(location.line);
+    DocumentPart::Image image("svg+xml", "svg", imageData, text);
+    document.addPart( image );
+
+
+    DWORD bytesAvail = 0;
+    if( PeekNamedPipe(_stdErrReadHandle, NULL, 0, NULL, &bytesAvail, NULL) ) {
+        if( bytesAvail == 0 )
+            return true;
+        std::string error;
+        error.resize(bytesAvail);
 
         DWORD readBytes = 0;
-        bSuccess = ReadFile( g_hChildStd_OUT_Rd, buffer, 1024, &readBytes, NULL);
+        if( ReadFile( _stdErrReadHandle, &error[0], bytesAvail, &readBytes, NULL) ) {
+            std::vector<std::string> errorInfo;
+            split(error, '\n', errorInfo);
+            int lineNumber = 0;
+            std::string errorText;
 
-        if( ! bSuccess || readBytes == 0 )
-            break;
+            if( errorInfo.size() > 0 && errorInfo[0] == "ERROR" ) {
+                if( errorInfo.size() > 1 ) {
+                    try {
+                        lineNumber = std::stoi(errorInfo[1]);
+                    } catch(...) {
+                        return false;
+                    }
+                }
 
-        if( readBytes > 0 )
-        {
-            imageData.append(buffer, static_cast<size_t>(readBytes));
-        } else {
-            break;
+                for( int i = 2; i < errorInfo.size(); i++ ) {
+                    errorText += errorInfo[i];
+                }
+                FileLocation l = location;
+                l.line += lineNumber + 1;
+                _errors.push_back({l, errorText});
+                return false;
+            }
         }
     }
 
-    WaitForSingleObject(piProcInfo.hProcess, INFINITE);
-
-    DocumentPart::Text text(location.line);
-    DocumentPart::Image image("png", "png", imageData, text);
     _cache.insert(std::make_pair(block, image));
-    document.addPart( image );
+
     return true;
 }
 
